a707f3dc9e4ec624b25963f90141784e9bb82b7e
CherylEThomas <cherylelysethomas@hotmail.co.uk>
2022-05-03 21:51:16 +0000

    concept files deleted; html, js and css files updated

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+let card = document.querySelectorAll('.cell')
+let front = document.querySelectorAll('.front')
+let container = document.querySelector('.container')
+let score = document.querySelector('.score span')
+
+shuffleImage()
+clicking()
+function shuffleImage(){
+
+    card.forEach(c=>{
+
+        const num = [...Array(card.length).keys()]
+        const random = Math.floor(Math.random()*card.length)
+
+        c.style.order = num[random]
+    })
+}
+
+function clicking(){
+
+    for(let i =0; i<card.length; i++){
+
+        card[i].addEventListener('click' ,()=>{
+
+            front[i].classList.add('flip')
+           const filppedCard = document.querySelectorAll('.flip')
+
+            if(filppedCard.length == 2){
+
+                container.style.pointerEvents ='none'
+                
+                setInterval(() => {
+                    
+                    container.style.pointerEvents ='all'
+                }, 1000);
+ 
+                match(filppedCard[0] , filppedCard[1])
+            }
+        })
+    }
+}
+
+function match(cardOne , cardTwo){
+
+    if(cardOne.dataset.index == cardTwo.dataset.index){
+
+        score.innerHTML = parseInt(score.innerHTML) + 1
+       
+        cardOne.classList.remove('flip') 
+        cardTwo.classList.remove('flip') 
+
+        cardOne.classList.add('match')
+        cardTwo.classList.add('match')
+
+    }else{
+
+        setTimeout(() => {
+            
+            cardOne.classList.remove('flip') 
+            cardTwo.classList.remove('flip') 
+        }, 1000);
+    }
+}
